927aa7dbeeefcb718b5ee522c8bebc16828087c0
RohitMunjal25 <mobilemunjal@gmail.com>
2025-04-16 23:05:15 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class q7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int num=sc.nextInt();
+        if(num==0){
+            System.out.println("the num is 0");
+
+        }
+        if(num>0){
+            System.out.println("the number is positive");
+        }
+        else{
+            System.out.println("the no is negative");
+        }
+        }
+    }
+    
+
---
+++ q5.java
@@ -0,0 +1,13 @@
+public class q10{
+    public static void main(String[] args) {
+        int rows=4;
+       int num=1;
+       for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(j+"");
+            
+        }
+        System.out.print("\n");
+       }
+    }
+}
